d6098cffce345ed63a1f4f680469ce56b774033e
rong142 <C109118142@nkust.edu.tw>
2024-07-12 11:38:27 +0800

    esp32_04

---
+++ 04_PIR2/04_PIR2.ino
@@ -0,0 +1,33 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(17,INPUT);
+  pinMode(16,OUTPUT);
+
+  pinMode(15,OUTPUT);
+  pinMode(2,OUTPUT);
+  pinMode(4,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = digitalRead(17);
+  if (value == HIGH){
+    //Serial.println("")
+    digitalWrite(4,LOW);
+    digitalWrite(15,HIGH);
+    Serial.println("有人ㄚㄚㄚㄚㄚ");
+
+    digitalWrite(16,HIGH);
+    delay(1000);
+    digitalWrite(16,LOW);
+    
+  }else{
+    digitalWrite(15,LOW);
+    digitalWrite(4,HIGH);
+    Serial.println("沒人了");
+    delay(1000);
+  }
+//  Serial.println(value);
+  delay(500);
+}
